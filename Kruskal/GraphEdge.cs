c4371f388308f2e76d1567393e64c63339a46118
Semen <45079454+SleepIkki@users.noreply.github.com>
2019-11-24 19:25:02 +0300

    Easier than I expected... it is ready.

---
+++ Kruskal/GraphEdge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kruskal
+{
+    class GraphEdge
+    {
+        public int Point1 { get; }
+        public int Point2 { get; }
+        public int Weight { get; set; }
+
+        public GraphEdge(int p1, int p2, int weight)
+        {
+            Point1 = p1;
+            Point2 = p2;
+            Weight = weight;
+        }
+    }
+}
---
+++ Kruskal/KruskalSpanningTree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kruskal
+{
+    class KruskalSpanningTree
+    {
+        public List<GraphEdge> NotUsedEdges { get; } = new List<GraphEdge>();
+        public List<GraphEdge> SpanningTree { get; } = new List<GraphEdge>();
+        List<int> UsedPoints = new List<int>();
+        List<int> NotUsedPoints = new List<int>();
+
+        public KruskalSpanningTree(List<GraphEdge> graph)
+        {
+            NotUsedEdges = graph;
+            NotUsedPoints = GetPoints(graph);
+        }
+
+        public void Kruskal()
+        {
+            while (NotUsedPoints.Count > 0)
+            {
+                GraphEdge minEdge = NotUsedEdges[0];
+
+                for (int i = 0; i < NotUsedEdges.Count; ++i)
+                {
+                    if (minEdge.Weight > NotUsedEdges[i].Weight)
+                        minEdge = NotUsedEdges[i];
+                }
+
+                if (!UsedPoints.Contains(minEdge.Point1) || !UsedPoints.Contains(minEdge.Point2))
+                {
+
+                    SpanningTree.Add(minEdge);
+
+                    if (!UsedPoints.Contains(minEdge.Point1))
+                    {
+                        UsedPoints.Add(minEdge.Point1);
+                        NotUsedPoints.Remove(minEdge.Point1);
+                    }
+
+                    if (!UsedPoints.Contains(minEdge.Point2))
+                    {
+                        UsedPoints.Add(minEdge.Point2);
+                        NotUsedPoints.Remove(minEdge.Point2);
+                    }   
+                }
+                NotUsedEdges.Remove(minEdge);
+            }
+        }
+
+        public List<int> GetPoints(List<GraphEdge> graph)
+        {
+            List<int> points = new List<int>();
+
+            for (int i = 0; i < graph.Count; ++i)
+            {
+                if (points == null || !points.Contains(graph[i].Point1))
+                    points.Add(graph[i].Point1);
+                if (points == null || !points.Contains(graph[i].Point2))
+                    points.Add(graph[i].Point2);
+            }
+
+            return points;
+        }
+
+        public override string ToString()
+        {
+            string spanningTree = "";
+            for (int i = 0; i < SpanningTree.Count; ++i)
+                spanningTree += ("Point1: " + SpanningTree[i].Point1 +
+                    "\tPoint2: " + SpanningTree[i].Point2 +
+                    "\tWeight: " + SpanningTree[i].Weight) + "\n";
+            return spanningTree;
+        }
+    }
+}
